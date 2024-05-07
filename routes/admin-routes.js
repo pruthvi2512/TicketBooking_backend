5f071909f7cdcc36527e2bf43a8049d428cb6d78
Rutuja Tarapurkar <rutujatarapurkar8@gmail.com>
2024-05-07 14:41:57 +0530

    add port

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const adminrouter=express.Router();
-import { addadmin,adminlogin } from "../controllers/admin-cont";
+import { addadmin,adminlogin } from "../controllers/admin-cont.js";
 adminrouter.post("/signup",addadmin );
 adminrouter.post("/login",adminlogin);
 
